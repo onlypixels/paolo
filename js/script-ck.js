43f9e667016ef6535b961d5f93b5a76e13ab2e46
Finbar Sheahan <finbar.sheahan@gmail.com>
2012-06-21 22:34:31 +0100

    adding some style

---
+++ js/script-ck.js
@@ -0,0 +1,3 @@
+/* Author:
+
+*/function init(){$(".quote").Paolo()}$(".generatorButton").click(function(){$(".quote").Paolo("generateQuote");return!1});$(document).ready(init);
---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Author:
+
+*/
+
+
+
+
+function init() {
+    $(".quote").Paolo();
+}
+
+$(".generatorButton").click(function () { $(".quote").Paolo("generateQuote"); return false; });
+$(document).ready(init);
+// Window load event used just in case window height is dependant upon images
+ 
